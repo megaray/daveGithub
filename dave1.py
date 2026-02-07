b42fc7a49907b71a72e3f8489e0c03f275441e4c
Louis Deglon <louisdeglon@gmail.com>
2026-02-07 16:44:41 +0100

    full skill monsieur le juge

--- dave1.py
+++ dave1.py
@@ -1,1 +1,16 @@
 print("J'apprend git!!!")
+
+print("C'est super!!!")
+if True:
+    print("C'est génial!!!")
+else:
+    print("C'est nul!!!")   
+
+function test():
+    print("C'est une fonction!!!")
+function depthSorting():
+    print("C'est une fonction de tri de profondeur!!!")
+    if True:
+        print("C'est génial!!!")
+    else:
+        print("C'est nul!!!")  
